29de9ac762cd69a236b0559cc0b5a87fc00cd9be
Ashish Rathi <rathiashish7079@gmail.com>
2026-08-20 00:21:45 +0530

    update env

--- backend/src/main/java/com/ecolift/config/WebSocketConfig.java
+++ backend/src/main/java/com/ecolift/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.ecolift.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
@@ -9,13 +10,14 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
+    @Value("${import.env.APP_BASE_URL}")
+    private final String appBaseUrl;
     private final JwtHandshakeInterceptor jwtHandshakeInterceptor;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173")
+                .setAllowedOrigins("http://localhost:5173", appBaseUrl)
                 .addInterceptors(jwtHandshakeInterceptor)
                 .setHandshakeHandler(new UserHandshakeHandler())
                 .withSockJS();
